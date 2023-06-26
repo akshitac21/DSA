1c4002708c63a5813bd9cefd677c870b91972380
Akshita Choudhury <aksxclint@gmail.com>
2023-06-26 14:32:09 +0300

    Longest consecutive subsequence

---
+++ 450Sheet/Array/longestConsecutiveSubsequence.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void solve(int arr[], int n){
+
+    unordered_set<int>set;
+    for(int i=0; i<n; i++){
+        set.insert(arr[i]);
+    }
+
+    int ans = 0;
+    int count = 0;
+
+    for(int i=0; i<n; i++){
+
+        if(set.find(arr[i]+1)==set.end()){
+            count =1;
+
+            while(set.find(arr[i]+count)!= set.end()){
+                count++;
+            }
+        }
+
+        ans = max(ans,count);
+    }
+
+    cout << ans;
+    
+    
+}
+
+int main(){
+
+    int arr[] = {2,6,1,9,4,5,3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    solve(arr,n);
+
+    return 0;
+
+}
