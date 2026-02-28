a4f2038e3e2e401995d5702c06d30970dd616647
Akshita <aksxclint@gmail.com>
2026-03-01 01:37:49 +0530

    alternate method for pattern 18

--- Extras/Patterns/18.cpp
+++ Extras/Patterns/18.cpp
@@ -5,10 +5,21 @@ int main(){
     int n; 
     cin>>n;
 
-    for(int i=0; i<n; i++){
-        for(char ch='E'-i; ch<='E'; ch++){
-            cout<<ch<<" ";
+    // for(int i=0; i<n; i++){
+    //     for(char ch='E'-i; ch<='E'; ch++){
+    //         cout<<ch<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    for (int i = 1; i <= n; i++)
+    {
+        char start = 'A' + n - i;
+        for (int j = 0; j < i; j++)
+        {
+            cout << start << " ";
+            start++;
         }
-        cout<<endl;
+        cout << endl;
     }
 }
